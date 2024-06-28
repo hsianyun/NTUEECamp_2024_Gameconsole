d44945017ee884d3138c363d051c505c83cba5f5
Your Name <you@example.com>
2024-06-28 20:07:55 +0800

    modify my_coponents(functions)

--- Speaker/pipeline_spiffs_mp3/components/my_component/include/my_component.h
+++ Speaker/pipeline_spiffs_mp3/components/my_component/include/my_component.h
@@ -4,37 +4,39 @@
 #include "audio_pipeline.h"
 #include "esp_peripherals.h"
 
-typedef enum {
-    AUDIO_FIRST = 0,
-    AUDIO_SECOND,
-    AUDIO_NUM
-} audio_index_t;
-
-#define RINGBUF_INPUT_SIZE (6 * 1024)
-
-#define AUDIO_INDEX_ITERATE(index) for (int index = 0; index < AUDIO_NUM; index++)
-
 // static const char *TAG = "MY_COMPONENT";
 
-// 函數原型
+// functions
+// 初始化音頻系統
 void initialize_audio_system();
 
+// 設置播放檔案
 void set_mp3(const char *file_path);
 
+// 啟動播放系統
 void start_mp3();
 
+// 播放
 void play_mp3();
 
+// 隨時處理音頻播放狀態
+// 當音樂結束時 replay_audio();
 void handle_audio_events();
 
+// 打斷現在音樂進行並重複播放
 void replay_audio();
 
+// 暫停
 void pause_audio();
 
+// 恢復播放
 void resume_audio();
 
+// 停止播放並銷毀現有進程
 void stop_audio();
 
+// 設置音量大小
+// volume 範圍有待商榷
 void set_volume(int volume);
 
 #endif // MY_COMPONENT_H
