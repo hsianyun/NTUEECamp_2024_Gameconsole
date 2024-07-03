89d50d53a370e3bb563198ed654b03f85719b6c0
Ryan <113010586+b11901040@users.noreply.github.com>
2024-07-03 22:07:55 +0800

    move code into functions

--- Main/joystick/main/main.c
+++ Main/joystick/main/main.c
@@ -5,44 +5,54 @@
 #include "esp_flash.h"
 #include "esp_system.h"
 #include "driver/adc.h"
+#include <driver/gpio.h>
 
+// define as global variables
+int xPin = ADC1_CHANNEL_7;  
+int yPin = ADC1_CHANNEL_6;  
 
-int xPin = ADC1_CHANNEL_4;  
-int yPin = ADC1_CHANNEL_7;  
-int zPin = ADC1_CHANNEL_6;
-  
-int xval=0;    //Ｘ軸變數
-int yval=0;    //Ｙ軸變數
-int zval=0;    //Ｚ軸變數
+int x_val = 0;
+int y_val = 0;
+int z_val = 0;
+int previous_z_val = 0;
 
-
-void setup() {
+// setup
+void setup_joystick() {
     adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(xPin, ADC_ATTEN_DB_11);
-    adc1_config_channel_atten(yPin, ADC_ATTEN_DB_11);
-    adc1_config_channel_atten(zPin, ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(xPin, ADC_ATTEN_DB_12);
+    adc1_config_channel_atten(yPin, ADC_ATTEN_DB_12);
+    gpio_set_direction(GPIO_NUM_39, GPIO_MODE_INPUT);
 }
 
-void loop() {
-    xval = adc1_get_raw(xPin);  
-    yval = adc1_get_raw(yPin);  
-    zval = adc1_get_raw(zPin);  
+// get x value
+inline int get_joystick_x() {
+    return adc1_get_raw(xPin);  
+}
 
+// get y value
+inline int get_joystick_y() {
+    return adc1_get_raw(yPin);  
+}
 
-  char buf[100];
-  sprintf(buf, "VRx=%d, VRy=%d, SW=%d", xval, yval, zval);
-  printf("%s\n", buf);
+// get z value
+inline int get_joystick_z() {
+    return gpio_get_level(GPIO_NUM_39);
+}
 
-  vTaskDelay(100 / portTICK_PERIOD_MS);
+inline void set_previous_z_val() {
+    previous_z_val = z_val;
 }
 
 void app_main(void)
 {
-    setup();
+    setup_joystick();
     
     while (true)
     {
-        loop();
+        set_previous_z_val();
+        x_val = get_joystick_x();
+        y_val = get_joystick_y();
+        z_val = get_joystick_z();
     }
     
 }
