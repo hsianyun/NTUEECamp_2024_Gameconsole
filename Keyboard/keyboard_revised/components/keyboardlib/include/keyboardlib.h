55481e9f683fd329bf75704cd649bdcfde6b9379
yym0807 <kevinyeh05@gmail.com>
2024-07-05 13:17:03 +0800

    debouncing and handle ultimate skills in Keyboard/keyboard_revised/keyboardlib

--- Keyboard/keyboard_revised/components/keyboardlib/include/keyboardlib.h
+++ Keyboard/keyboard_revised/components/keyboardlib/include/keyboardlib.h
@@ -5,12 +5,17 @@
 
 #define NUM_OF_COL 3
 #define NUM_OF_ROW 2
+#define BOUNCE_DELAY_MS 40 // For debouncing
+#define ULT_DELAY_MS 1000 // maximum delay between keys of a ultimate skill 
+#define ULT_LEN 5 // length of ultimate skills
+#define ULT_NUM 2 // amount of ultimate skills
 
 void keyboardSetup(void);
 void detectKeyboard(void);
 void printKeyState(void);
 void printKeyPress(void);
 void printKeyRelease(void);
+int getUltimateSkill(void);
 
 const bool* getKeyState(void);
 
--- Keyboard/keyboard_revised/components/keyboardlib/src/keyboardlib.c
+++ Keyboard/keyboard_revised/components/keyboardlib/src/keyboardlib.c
@@ -1,5 +1,6 @@
 #include <driver/gpio.h>
 #include "keyboardlib.h"
+#include <time.h>
 
 #define GPIO_HIGH 1
 #define GPIO_LOW 0
@@ -9,6 +10,83 @@ const uint8_t COLUMN[NUM_OF_COL] = {GPIO_NUM_14, GPIO_NUM_12, GPIO_NUM_13};  // 
 
 bool mem1[NUM_OF_ROW*NUM_OF_COL] = {0}, mem2[NUM_OF_ROW*NUM_OF_COL] = {0};
 bool *curr_key_state = mem1, *last_key_state = mem2; // Array to store key states in current and last frame. 0 = not pressed, 1 = pressed
+clock_t timeOfChange[NUM_OF_ROW*NUM_OF_COL] = {0}; // Debouncing: array to store latest time of input change
+bool bouncing[NUM_OF_ROW*NUM_OF_COL] = {0}; // Array to store whether key is bouncing
+const int ULT_CODE[ULT_NUM][ULT_LEN] = {{0, 2, 3, 4, 1},
+                                        {1, 1, 4, 4, 2}}; // The codes to trigger ultimate skills
+int ultSkillExecute = -1; // the ultimate skill to be executed
+
+// Used in detectKeyboard() to handle changes of ultimate skill detection by key press
+void handleUltimateSkill(void){
+    // Set ultSkillExecute to be the index of ultimate skill triggered. -1 for no combination satisfied.
+    static int ultCount[ULT_NUM] = {0}; // Array to store current step of each ultimate skill
+    static clock_t lastTimeKeyPress = 0;
+    for(int i = 0; i < ULT_NUM; i++){
+        if(1000 * (clock() - lastTimeKeyPress) > ULT_DELAY_MS * CLOCKS_PER_SEC){
+            // click too slow
+            ultCount[i] = 0;
+        }
+        else{
+            bool keyPressBuffer[NUM_OF_ROW*NUM_OF_COL]; // store key pressed
+            for(int j = 0; j < NUM_OF_ROW*NUM_OF_COL; j++){
+                keyPressBuffer[j] = (curr_key_state[j] > last_key_state[j]);
+            }
+            /*Current design for multiple keys pressed at the same time: 
+                If any order of all the keys pressed (not repeated) correspond to
+                some of the next keys for an ultimate skill, or any order of some
+                keys pressed (not repeated) correspond to the rest of an ultimate
+                skill code, then consider a successful click of those keys.
+                Fails otherwise (ultCount reset to 0).
+            */
+            while(keyPressBuffer[ULT_CODE[i][ultCount[i]]]){
+                // current skill code step satisfied, check next one
+                keyPressBuffer[ULT_CODE[i][ultCount[i]]] = 0;
+                ultCount[i]++;
+                if(ultCount[i] == ULT_LEN){
+                    // successfully triggers the ultimate skill
+                    ultSkillExecute = i;
+                    ultCount[i] = 0;
+                    break;
+                }
+            }
+            for(int j = 0; j < NUM_OF_ROW*NUM_OF_COL; j++){
+                if(keyPressBuffer[j] && ultSkillExecute == -1){
+                    // fails, redetect from ultCount[i] = 0
+                    ultCount[i] = 0;
+                    for(int j = 0; j < NUM_OF_ROW*NUM_OF_COL; j++){
+                        keyPressBuffer[j] = (curr_key_state[j] > last_key_state[j]);
+                    }
+                    while(keyPressBuffer[ULT_CODE[i][ultCount[i]]]){
+                        // current skill code step satisfied, check next one
+                        keyPressBuffer[ULT_CODE[i][ultCount[i]]] = 0;
+                        ultCount[i]++;
+                    }
+                    for(int j = 0; j < NUM_OF_ROW*NUM_OF_COL; j++){
+                        if(keyPressBuffer[j]){
+                            // fails
+                            ultCount[i] = 0;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    if(ultSkillExecute != -1){
+        // a ultimate skill triggered
+        for(int i = 0; i < ULT_NUM; i++){
+            ultCount[i] = 0;
+        }
+    }
+    
+    for(int i = 0; i < NUM_OF_ROW*NUM_OF_COL; i++){
+        if(curr_key_state[i] > last_key_state[i]){
+            // some key pressed
+            lastTimeKeyPress = clock();
+            break;
+        }
+    }
+}
 
 void keyboardSetup(void){
     // Initialize row pins as outputs and set them to HIGH
@@ -31,17 +109,39 @@ void detectKeyboard(void){
     bool *tmp = last_key_state;
     last_key_state = curr_key_state;
     curr_key_state = tmp;
-
+    clock_t currentTime = clock();
     for(int i = 0; i < NUM_OF_ROW; i++){
         gpio_set_level(ROW[i], GPIO_LOW);   // Activate the current row (set LOW)
 
         // Iterate through each column to check key status
         for(int j = 0; j < NUM_OF_COL; j++){
-            curr_key_state[i*NUM_OF_COL + j] = !gpio_get_level(COLUMN[j]);  // Set the key state in the current frame
+            if(bouncing[i*NUM_OF_COL + j]){
+                if(curr_key_state[i*NUM_OF_COL + j] != gpio_get_level(COLUMN[j])){
+                    // Bounce back, treat as no bounce
+                    bouncing[i*NUM_OF_COL + j] = 0;
+                    curr_key_state[i*NUM_OF_COL + j] = last_key_state[i*NUM_OF_COL + j];
+                }
+                else if(1000 * (currentTime - timeOfChange[i*NUM_OF_COL + j]) < BOUNCE_DELAY_MS * CLOCKS_PER_SEC){
+                    // Waiting for bouncing ends
+                    curr_key_state[i*NUM_OF_COL + j] = last_key_state[i*NUM_OF_COL + j];
+                }
+                else{
+                    // Bouncing ends
+                    bouncing[i*NUM_OF_COL + j] = 0;
+                    curr_key_state[i*NUM_OF_COL + j] = !gpio_get_level(COLUMN[j]);  // Set the key state in the current frame
+                }
+            }
+            else{
+                curr_key_state[i*NUM_OF_COL + j] = last_key_state[i*NUM_OF_COL + j];
+                if(curr_key_state[i*NUM_OF_COL + j] != (!gpio_get_level(COLUMN[j]))){
+                    // input changes, start bouncing delay
+                    bouncing[i*NUM_OF_COL + j] = 1;
+                }
+            }
         }
-
         gpio_set_level(ROW[i], GPIO_HIGH);   // Deactivate the current row (set HIGH)
     }
+    handleUltimateSkill();
 }
 
 // Test
@@ -77,4 +177,11 @@ void printKeyRelease(void)
         printf("\n");
     }
     //printf("\n");
+}
+
+// return the index of ultimate skill to be executed and set ultSkillExecute back to -1
+int getUltimateSkill(void){
+    int ult = ultSkillExecute;
+    ultSkillExecute = -1;
+    return ult;
 }
