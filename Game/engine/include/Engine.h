e6de6b170a959c54e0d41074ea1fe4917906931b
SUPAMADAFAKABIGBEE <99779773+SUPAMADAFAKABIGBEE@users.noreply.github.com>
2024-07-07 13:58:40 +0800

    organize renderManager, and provide sample code for game design, also modify .gitignore (#32)

---
+++ Game/engine/include/Engine.h
@@ -0,0 +1,17 @@
+/*
+    Simple Game Engine for ESP32
+*/
+
+/*
+    Engine.h
+
+    Main include header for the whole engine library.
+*/
+
+#ifndef _ENGINE_H_
+#define _ENGINE_H_
+
+#include "ResourceManager.h"
+#include "RenderManager.h"
+
+#endif /* _ENGINE_H_ */
